66fde959e25a5c2a5fe026c6c8d70d49174dd592
aid848 <33972075+aid848@users.noreply.github.com>
2020-07-04 23:08:36 -0700

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,76 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {Button, Form, FormGroup, Input, Label} from 'reactstrap';
+import axios from 'axios';
+const url:string = "http://127.0.0.1:8080";
 
-function App() {
+
+function App(this: any) {
+
+    const [btnTxt, setBtnTxt] = useState('click me!');
+    const [loggedin, setlogin] = useState(0);
+    const [username, setusername] = useState("");
+    const [password, setPassword] = useState("");
+    const [loginMessage, setLoginMessage] = useState("Please Login");
+
+    if (loggedin === 0) {
+     return loginscreen(setlogin,username,setusername, password, setPassword, loginMessage, setLoginMessage);
+    }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Button color="danger" onClick={() => {
+            if (btnTxt === "click me!") {
+                // @ts-ignore
+                setBtnTxt('clicked')
+            }else {
+                // @ts-ignore
+                setBtnTxt("click me!")
+            }
+        }}>{btnTxt}</Button>
     </div>
   );
+}
+
+function loginscreen(setlogin:any,username:any,setusername:any, password:any, setPassword:any, loginMessage:any, setLoginMessage:any) {
+
+return (
+    <div className="login">
+        <p>{loginMessage}</p>
+        <Form onSubmit = {() => {
+            console.log()
+        }}>
+            <FormGroup>
+                <Label for="username">username</Label>
+                <Input type="text" id="username" onChange={(chg) => {
+                    chg.preventDefault();
+                    let x = chg.target.value
+                    console.log(x);
+                    setusername(x);
+                }}/>
+            </FormGroup>
+            <FormGroup>
+                <Label for="password">password</Label>
+                <Input type="password" id="password" onChange={(chg) => {
+                    chg.preventDefault();
+                    let x = chg.target.value
+                    console.log(x);
+                    setPassword(x);
+                }}/>
+            </FormGroup>
+        </Form>
+        <button onClick={() =>
+        {
+            console.log("trying logging in: " + username + " Pass: " + password);
+            return axios.post(url+"/login", {username: username, password:password}).then((res) => {
+                console.log(res);
+            }).catch((err) => {
+                console.log("err:" + err);
+            })
+        }
+        }>login</button>
+    </div>
+)
 }
 
 export default App;
